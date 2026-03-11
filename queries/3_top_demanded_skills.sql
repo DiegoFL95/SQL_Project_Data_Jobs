e63c5d6920ad93b6e82810cfec4ac5f0ac090c3f
DiegoFL95 <diegofarresl@gmail.com>
2026-03-11 20:32:29 +0100

    push query 3

--- queries/3_top_demanded_skills.sql
+++ queries/3_top_demanded_skills.sql
@@ -2,19 +2,39 @@
 Question: What are the most in-demand skills for data analysts?
 - Join job posting to inner join table similar to query 2
 - Identify the top 5 in-demand skills for data analysts.
+- Identify the top 5 in-demand skills for business analyst.
 - Focus on all job postings.
 - Why? Retrives top 5 skills with the highest demand in the job market,
 which can help data analysts understand which skills are most valuable for their career development and job prospects.
         providing insight into the most valuable skills for job seekers.
-
 */  
 
-SELECT *
-FROM   job_postings_fact
-INNER JOIN skills_job_dim ON job_posting_fact.job_id = skills_job_dim.job_id
-Inner JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
-LIMIT 5
-
-SELECT * 
-FROM job_postings_fact
+-- top 5 in demand skills for a data analyst
+SELECT 
+        Skills,
+        COUNT(skills_job_dim.job_id) AS demand_count
+FROM  job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE 
+        job_title_short = 'Data Analyst'
+GROUP BY
+        skills
+ORDER BY 
+        demand_count DESC
+LIMIT 5;
+ 
+-- top 5 in demand skills for a business analyst
+SELECT 
+        Skills,
+        COUNT(skills_job_dim.job_id) AS demand_count
+FROM  job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE 
+        job_title_short = 'Business Analyst'
+GROUP BY
+        skills
+ORDER BY 
+        demand_count DESC
 LIMIT 5;
