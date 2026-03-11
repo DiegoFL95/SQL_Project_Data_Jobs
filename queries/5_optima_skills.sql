0b7a80b15d49fc43edf3b3ccd8faf04963423590
DiegoFL95 <diegofarresl@gmail.com>
2026-03-11 21:05:13 +0100

    push 5

---
+++ queries/5_optima_skills.sql
@@ -0,0 +1,43 @@
+/* 
+Answer: What are the most optimal skills to learn (Aka it's in high demand and high-paying skill)
+- Identify skills in high demand and asociated with high average salaries for Data Analyst and Business Analyst
+- Concentrates on remote positions for specific salaries
+- Why? targets skills that offer job security (high demand) and financial benefits (hig salaries),
+    offering strategic insight for career development in data analysis
+*/
+
+WITH skills_demand AS (
+    SELECT 
+        skills_dim.skills_id,
+        skills_dim.Skills,
+        COUNT(skills_job_dim.job_id) AS demand_count
+    FROM  job_postings_fact
+    INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+    INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+    WHERE 
+        job_title_short = 'Data Analyst'
+        AND salary_year_avg IS NOT NULL
+    GROUP BY
+        skill_id
+), average_salary AS (
+    SELECT 
+        skills_job_dim.skills_id,
+        ROUND(AVG(salary_year_avg), 0) AS avg_salary
+    FROM  job_postings_fact
+    INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+    INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+    WHERE 
+            job_title_short = 'Business Analyst'
+            AND salary_year_avg IS NOT NULL
+    GROUP BY
+        skill_id
+)
+
+SELECT
+    skill_demand.skill_id,
+    skill_demand.skills,
+    demand_count,
+    avg_salary
+FROM 
+    skills_demand
+INNER JOIN average_salary ON skills_demand.skill_id = average_salary.skill_id
