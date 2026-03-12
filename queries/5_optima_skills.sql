d2eadfdc937343257d0baba33e8f2b34f5a9d7f8
DiegoFL95 <diegofarresl@gmail.com>
2026-03-12 09:49:20 +0100

    push query 5

--- queries/5_optima_skills.sql
+++ queries/5_optima_skills.sql
@@ -8,7 +8,7 @@ Answer: What are the most optimal skills to learn (Aka it's in high demand and h
 
 WITH skills_demand AS (
     SELECT 
-        skills_dim.skills_id,
+        skills_dim.skill_id,
         skills_dim.Skills,
         COUNT(skills_job_dim.job_id) AS demand_count
     FROM  job_postings_fact
@@ -18,10 +18,10 @@ WITH skills_demand AS (
         job_title_short = 'Data Analyst'
         AND salary_year_avg IS NOT NULL
     GROUP BY
-        skill_id
+        skills_dim.skill_id
 ), average_salary AS (
     SELECT 
-        skills_job_dim.skills_id,
+        skills_job_dim.skill_id,
         ROUND(AVG(salary_year_avg), 0) AS avg_salary
     FROM  job_postings_fact
     INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
@@ -30,14 +30,19 @@ WITH skills_demand AS (
             job_title_short = 'Business Analyst'
             AND salary_year_avg IS NOT NULL
     GROUP BY
-        skill_id
+        skills_job_dim.skill_id
 )
 
 SELECT
-    skill_demand.skill_id,
-    skill_demand.skills,
+    skills_demand.skill_id,
+    skills_demand.skills,
     demand_count,
     avg_salary
 FROM 
     skills_demand
 INNER JOIN average_salary ON skills_demand.skill_id = average_salary.skill_id
+ORDER BY   
+   -- avg_salary DESC,
+    demand_count DESC,
+    avg_salary DESC
+LIMIT 10;
